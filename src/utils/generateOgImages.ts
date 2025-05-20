503b08b35f1f9e2a1d77e879ee91f69dea1f63ac
Mohammed Nasser Helal <41899209+elweday1@users.noreply.github.com>
2025-05-20 23:13:38 +0300

    Update generateOgImages.ts

--- src/utils/generateOgImages.ts
+++ src/utils/generateOgImages.ts
@@ -5,45 +5,11 @@ import generateOG from "./og-templates/cover";
 import siteOgImage from "./og-templates/site";
 import { type Collection } from "../types"; 
 
-const fetchFonts = async () => {
-  // Regular Font
-  const logoFontFile = await fetch(SITE.website+ "/fonts/batman.ttf");
-  const logoFont: ArrayBuffer = await logoFontFile.arrayBuffer();
-  const textFontRegularFile = await fetch(SITE.website+ "/fonts/Kalam-Regular.ttf");
-  const textFontRegular: ArrayBuffer = await textFontRegularFile.arrayBuffer();
-  const textFontBoldFile = await fetch(SITE.website+ "/fonts/Kalam-Bold.ttf");
-  const textFontBold: ArrayBuffer = await textFontBoldFile.arrayBuffer();
 
-
-  return { logoFont, textFontRegular, textFontBold };
-};
-
-const { logoFont, textFontRegular, textFontBold } = await fetchFonts();
 
 const options: SatoriOptions = {
   width: 1200,
   height: 630,
-  embedFont: true,
-  fonts: [
-    {
-      name: "batman",
-      data: logoFont,
-      weight: 400,
-      style: "normal",
-    },
-    {
-      name: "kalam",
-      data: textFontRegular,
-      weight: 400,
-      style: "normal",
-    },
-    {
-      name: "kalam-bold",
-      data: textFontBold,
-      weight: 700,
-      style: "normal",
-    },
-  ],
 };
 
 function svgBufferToPngBuffer(svg: string) {
