f1ee865e9a1ac5383313f11e84e968f6e8ecc84d
Mohammed Nasser Helal <41899209+elweday1@users.noreply.github.com>
2025-05-20 23:18:55 +0300

    Update index.png.ts

--- src/pages/blog/[slug]/index.png.ts
+++ src/pages/blog/[slug]/index.png.ts
@@ -1,16 +1,1 @@
-import type { APIRoute } from "astro";
-import { getCollection, type CollectionEntry } from "astro:content";
-import { generateOGImage } from "@utils/generateOgImages";
 
-export async function getStaticPaths() {
-  const posts = await getCollection("blog");
-  return posts.map(post => ({
-    params: { slug: post.slug },
-    props: post,
-  }));
-}
-
-export const GET: APIRoute = async ({ props }) =>
-  new Response(await generateOGImage(props as CollectionEntry<"blog">), {
-    headers: { "Content-Type": "image/png" },
-  });
